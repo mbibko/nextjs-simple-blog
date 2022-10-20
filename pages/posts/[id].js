ddbb22ebe9565f2a5767ed8112368f456c24ab3e
mbibko <mbibko@gmail.com>
2022-10-20 12:28:13 +0300

    added: formatted date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from "next/head";
+import Date from "../../components/date";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -25,7 +26,7 @@ export default function Post({postData}) {
       <title>{postData.title}</title>
     </Head>
     <h1>{postData.title}</h1>
-    <p>{postData.date}</p>
+    <Date dateString={postData.date} />
     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>;
 }
